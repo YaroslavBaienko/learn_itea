d345888cc49dfbc6488f6c86cdbce02172175a01
Yaroslav Baienko <zerhug@gmail.com>
2022-09-10 12:35:19 +0300

    initial commit

---
+++ 001_09.09_intro.py
@@ -0,0 +1,10 @@
+import sys
+
+
+print("Hello, world!")
+a = b = [1, 2, 3, 4, 5, 6]
+print(id(a), id(b))
+print(sys.getrefcount(a))
+
+
+print(sys.getsizeof(a))
---
+++ examples/OOP_test.py
@@ -0,0 +1,8 @@
+class TestClass:
+    version = 1
+
+
+test1 = TestClass()
+test2 = TestClass()
+
+print(id(test1), id(test2))
