42fe5172ba2533e9160544764aa20823561b7e2e
Yaroslav Baienko <zerhug@gmail.com>
2022-10-12 23:52:43 +0300

    solved codewars

---
+++ examples/codewars_calculate_BMI.py
@@ -0,0 +1,21 @@
+# Write function bmi that calculates body mass index (bmi = weight / height2).
+#
+# if bmi <= 18.5 return "Underweight"
+#
+# if bmi <= 25.0 return "Normal"
+#
+# if bmi <= 30.0 return "Overweight"
+#
+# if bmi > 30 return "Obese"
+def bmi(weight, height):
+    bmi = weight / height ** 2
+    if bmi <= 18.5:
+        return "Underweight"
+    if bmi <= 25.0:
+        return "Normal"
+    if bmi <= 30.0:
+        return "Overweight"
+    if bmi > 30:
+        return "Obese"
+
+print(bmi(72, 1.72))
---
+++ examples/codewars_disemvowel _trolls.py
@@ -0,0 +1,17 @@
+# A common way to deal with this situation is to remove all of the vowels from the trolls' comments, neutralizing the
+# threat.
+#
+# Your task is to write a function that takes a string and return a new string with all vowels removed.
+#
+# For example, the string "This website is for losers LOL!" would become "Ths wbst s fr lsrs LL!".
+#
+# Note: for this kata y isn't considered a vowel.
+
+def disemvowel(string_):
+    vowels = 'a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'
+    for vowel in vowels:
+      string_ = string_.replace(vowel, '')
+    return string_
+
+
+print(disemvowel('Hell wrold'))
